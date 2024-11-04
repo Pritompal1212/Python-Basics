44400b42e5e19eb58562efd501eef371788b9d05
Pritompal1212 <mspritom111@gmail.com>
2024-11-04 22:07:44 +0530

    ok

--- inverted_ptrn.py
+++ inverted_ptrn.py
@@ -1,6 +1,16 @@
-n=5
+# n=5
 
-for i in range(n):
-    print(" " * i, end="")
-    print("*" *(n-i))
+# for i in range(n):
+#     print(" " * i, end="")
+#     print("*" *(n-i))
     
+    
+#see an0ther way
+
+n=5
+for i in range(n):
+    for j in range(i):
+        print(" ",end="")
+    for k in range(n-i):
+        print("*",end="")
+    print()
